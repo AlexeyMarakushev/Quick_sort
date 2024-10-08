3f5c897d2225d88e6ccb1f758d53fd7d88b05daf
AlexeyMarakushev <alesha.marak@gmail.com>
2024-10-09 01:23:58 +0300

    Third commit

--- main.cpp
+++ main.cpp
@@ -2,14 +2,11 @@
 #include <iostream>
 int main(){
 	Item **items;
-	size_t count = 5;
+	size_t count = 2;
 	Real *A;
 	A = new Real[count];
-	A[0].Real.a = 3.0;
-	A[1].Real.a = 6.0;
-	A[2].Real.a = 4.0;
-	A[3].Real.a = 5.0;
-	A[4].Real.a = 1.0;
+	A[0].a = 3.0;
+	A[1].a = 6.0;
 	for(size_t i = 0; i < count; i++){
 		*items[i] = A[i];
 	}
